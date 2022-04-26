0c60da3b20613e8b05e08ad5d7dee42746fadc5e
maxwell2711 <maxwellog2711@gmail.com>
2022-04-25 23:03:48 -0400

    Bug fix/cleanup

--- src/helpers.c
+++ src/helpers.c
@@ -4,10 +4,11 @@
 
 void CheckInput() {
     //Check input on the left
-    if(PINE&&(1<<PE2)) {
+    if(!(PINE&(1<<PE2))) {
         SetOCR1A(OCR1A-1); //reduce pulse width by 1
-    } else if (PINE&&(1<<PE3)) { //check right input
-        SetOCR1A(ORC1A+1); //increase pulse width by 1
+    }
+    if (!(PINE&(1<<PE3))) { //check right input
+        SetOCR1A(OCR1A+1); //increase pulse width by 1
     }
 }
 
@@ -16,7 +17,7 @@ void JoystickInit() {
     DDRE &= !(1<<PE2); //indicate input on the left joystick
     PORTE |= (1<<PE2); //enable pull-up
 
-    //initialize left joystick on PE3
-    DDRE &= !(1<<PE3); //indicate input on the left joystick
+    //initialize right joystick on PE3
+    DDRE &= !(1<<PE3); //indicate input on the right joystick
     PORTE |= (1<<PE3); //enable pull-up
 }
--- src/proj4main.c
+++ src/proj4main.c
@@ -2,18 +2,22 @@
 #include "timer.h"
 #include "util/delay.h"
 #include "avr/interrupt.h"
+#include "helpers.h"
 #include <xc.h>
 
 void main(){
     MusicSetupPort();
     MusicSetupTimer1();
 	LCD_Init();
+    JoystickInit();
 
 	MusicSetNote(16667/2,1500); //set note
-    while(1) {  
+    while(1) {
         //refresh the LCD
 		LCD_WriteDigit((OCR1A/10)+'0',4);
 		LCD_WriteDigit((OCR1A%10)+'0',5);
+
+        CheckInput(); //check for an input and adjust OCR1A
         
         PORTB ^= (1 << PB0); //toggle LED
         _delay_ms(1000);
